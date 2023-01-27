22fe2ec0d5052a2b46cb1239b2c9085e570c8664
yaskou <anmitutarow@gmail.com>
2023-01-27 21:57:17 +0900

    feat: Add a function to convert hiragana to numbers

--- src/LeaFs/Library.fs
+++ src/LeaFs/Library.fs
@@ -1,5 +1,15 @@
-﻿namespace LeaFs
+﻿module LeaFs
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+let convertHiragana (name: string) =
+    name
+    |> Seq.toList
+    |> List.map (fun character ->
+        match character with
+        | character when "あかさたなはまやらわがざだばぱゃ".Contains character -> 1
+        | character when "いきしちにひみりぎじぢびぴ".Contains character -> 2
+        | character when "うくすつぬふむゆるをぐずづっぶぷゅ".Contains character -> 3
+        | character when "えけせてねへめれげぜでべぺ".Contains character -> 4
+        | character when "おこそとのほもよろんごぞどぼぽょ".Contains character -> 5
+        | _ -> 0
+    )
+    |> List.filter (fun i -> i <> 0)
