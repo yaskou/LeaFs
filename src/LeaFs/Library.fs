83ba9d7413673f063a57405831bb2228b9920301
yaskou <anmitutarow@gmail.com>
2023-01-29 12:16:52 +0900

    feat: Add a function to calculate the affinity between two people

--- src/LeaFs/Library.fs
+++ src/LeaFs/Library.fs
@@ -13,3 +13,20 @@ let convertHiragana (name: string) =
         | _ -> 0
     )
     |> List.filter (fun i -> i <> 0)
+
+let calculateAffinity (numbers: list<int>) =
+    let rec factorial n =
+        match n with
+        | 0 | 1 -> 1
+        | _ -> factorial (n - 1) * n
+    let binomialTheorem n r =
+        factorial n / (factorial r * factorial (n - r))
+
+    ("", [ numbers[..numbers.Length - 2]; numbers.Tail ])
+    ||> List.fold (fun acc numbers ->
+        acc + (numbers
+        |> List.reduce (fun prev i ->
+            prev + binomialTheorem (numbers.Length - 1) (List.findIndex ((=) i) numbers) * i
+        )
+        |> string |> Seq.last |> string)
+    )
